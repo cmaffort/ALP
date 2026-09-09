62d5992594e9c20ee38c82274e79e87750f6b1bc
cmaffort <maffort@gmail.com>
2026-09-09 17:54:22 -0300

    solução dos problemas 1001 e 1003 da lista 01

---
+++ lista01/p1001.c
@@ -0,0 +1,18 @@
+//48113946
+//11/03/2026 14:33:51
+
+#include <stdio.h>
+
+int main() {
+ 
+    int a, b, x;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    x = a + b;
+
+    printf("X = %d\n", x);
+
+    return 0;
+}
---
+++ lista01/p1003.c
@@ -0,0 +1,17 @@
+//48216014
+//18/03/2026 13:45:49
+
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    int soma = a + b;
+
+    printf("SOMA = %d\n", soma);
+
+    return 0;
+}
