ccdf83716424ddd08b5550526f4da841d2ac0af8
cmaffort <maffort@gmail.com>
2026-09-09 17:56:13 -0300

    solução do problema 1006 da lista 01

---
+++ lista01/p1006.c
@@ -0,0 +1,17 @@
+//48216605
+//18/03/2026 14:15:32
+
+#include <stdio.h>
+
+int main() {
+
+    double a, b, c;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    double media = (a * 2 + b * 3 + c * 5) / 10.0;
+
+    printf("MEDIA = %.1f\n", media);
+
+    return 0;
+}
